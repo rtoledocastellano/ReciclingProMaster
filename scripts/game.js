35ac9ee60f6aa6361115fcb0e323fe4955035771
ratoledo <rtoledocastellano@gmail.com>
2020-05-09 12:40:16 +0100

    Comments added on factorized functions

--- scripts/game.js
+++ scripts/game.js
@@ -1,8 +1,5 @@
 //Function from 1 to 4 to set the type of waste
 //1 - Papper; 2- Plastic; 3-Organic; 4-Cristal
-/*function random (num) {
-return Math.floor(Math.random()*num + 1);
-}*/
 
 //#canvas>div[class*=cube]
 
@@ -11,61 +8,37 @@ var self = this;
 this.points = 0;
 this.timeLeft = 30.00;
 this.downloadTimer;
+this.binType = ['papper','plastic','organic','cristal'];
 
 this.random = function(num) {
     return Math.floor(Math.random()*num + 1);
 }
 
 this.changeCube = function() {
-    var cube = document.getElementById("cube");
-    var rubishType = self.random(4);
-    cube.removeAttribute('class');
-    cube.setAttribute('value',`${rubishType}`);
-    switch(rubishType) {
-        case 1:
-            cube.setAttribute('class','cube-papper');
-            break;
-        case 2:
-            cube.setAttribute('class','cube-plastic');
-            break;
-        case 3:
-            cube.setAttribute('class','cube-organic');
-            break;
-        case 4:
-            cube.setAttribute('class','cube-cristal');
-            break;
-    }
+    var cube = document.getElementById("cube"); //Select the element in the DOM
+    var rubishType = self.random(4); //Create a new binType
+    cube.removeAttribute('class'); //Remove the current binClass
+    cube.setAttribute('value',`${rubishType}`); //Set the fixed value of the bin
+    cube.setAttribute('class',`cube-${self.binType[rubishType-1]}`) //Add the class
 }
 
 this.generateRubish = function() {
-    let parent = document.getElementById('canvas');
-    let firstChild = document.getElementById('cube');
-    let rubish = document.createElement('div');
-    let rubishType = self.random(4);
-    rubish.setAttribute('value',`${rubishType}`);
-    switch(rubishType) {
-        case 1:
-            rubish.setAttribute('class','rubish-papper');
-            break;
-        case 2:
-            rubish.setAttribute('class','rubish-plastic');
-            break;
-        case 3:
-            rubish.setAttribute('class','rubish-organic');
-            break;
-        case 4:
-            rubish.setAttribute('class','rubish-cristal');
-            break;
-    }    
-    parent.insertBefore(rubish,firstChild);
+    let parent = document.getElementById('canvas'); //Select the parentNode
+    let firstChild = document.getElementById('cube'); //Select firstChild
+    let rubish = document.createElement('div'); //Create the rubish
+    let rubishType = self.random(4); //Generate new binType
+    rubish.setAttribute('value',`${rubishType}`); //Add the fixed value
+    rubish.setAttribute('class',`rubish-${self.binType[rubishType-1]}`) //Add the class
+  
+    parent.insertBefore(rubish,firstChild); //All the elements will be inserted before the cube
     let top = self.random(350) + 80;
-    let left = self.random(1000) + 100;
+    let left = self.random(1000) + 100; //Generation of random position in the canvas
     rubish.style.top = top + 'px';
-    rubish.style.left = left + 'px';
-    self.activateRubish();
+    rubish.style.left = left + 'px'; //Draw it on the canvas
+    self.activateRubish(); //This functions active the eventListeners
 }
 
-this.removeItem = function(e) {
+this.removeItem = function(e) { //Remove the items once It are clicked
     let item = e.currentTarget;
     item.parentNode.removeChild(item);
 }
@@ -119,7 +92,7 @@ downloadTimer = setInterval(function(){
         document.getElementById('timer').innerHTML = `${self.timeLeft.toFixed(2)}`;
     }
 },10);
-this.trial1 = setInterval(this.changeCube,5000);
+this.trial1 = setInterval(this.changeCube,2000);
 this.trial2 = setInterval(this.generateRubish,1000);
 
 }
